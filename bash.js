d356e09b6ba576fcb489e57630173c64f97e5617
Tara Whirley <whirleyt@outlook.com>
2018-01-15 10:55:29 -0500

    pwd/date completed

---
+++ bash.js
@@ -0,0 +1,20 @@
+'use strict'
+
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+  if(cmd=== 'pwd'){
+    process.stdout.write('pwd: ' + process.cwd())
+  } else if (cmd ==='date'){
+    process.stdout.write('date: ' + new Date().toString())
+  }
+  //process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+  
+
+});
+
+var fs= require('fs');
+
