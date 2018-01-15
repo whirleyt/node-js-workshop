acb2f1e21e6e392d894af7108f5e176585768ab1
Tara Whirley <whirleyt@outlook.com>
2018-01-15 12:22:01 -0500

    subject line
    what happened
    
    [ticket: X}

--- command.js
+++ command.js
@@ -1,6 +1,8 @@
 module.exports = {
   cmdReader: function (data) {
-    var cmd = data.toString().trim(); // remove the newline
+    var cmds = data.toString().trim();
+    var cmd = cmds.split(' ')[0]
+    // remove the newline
     switch (cmd) {
       case 'pwd':
         pwd();
@@ -10,6 +12,9 @@ module.exports = {
         break;
       case 'ls':
         ls('.');
+        break;
+      case 'echo':
+        echo(cmds.slice(cmd.length + 1))
         break;
       default:
         break;
@@ -31,11 +36,23 @@ function date() {
   outIt('date', new Date().toString())
 }
 function ls(path) {
-  fs.readdir(path, function(err, files) {
+  fs.readdir(path, function (err, files) {
     if (err) throw err;
-    files.forEach(function(file) {
+    files.forEach(function (file) {
       process.stdout.write(file.toString() + "\n");
     })
   });
+}
+
+function echo(string) {
+  var words = string.split(' ')
+  words.forEach(function(word){
+    if (word[0] === '$') {
+      if (Object.keys(process.env).includes(word.slice(1))){
+        string = string.replace(word, process.env[word.slice(1)])
+      }
+    }
+  });
+  outIt('echo', string)
 }
 
