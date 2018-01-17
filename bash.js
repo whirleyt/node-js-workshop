07623a3c1401efb9ecaa709a855c05b5a4083d2e
Tara Whirley <whirleyt@outlook.com>
2018-01-17 16:52:30 -0500

    Completed

--- bash.js
+++ bash.js
@@ -1,21 +1,48 @@
-'use strict'
+'use strict';
 
-process.stdout.write('prompt > ');
+// constants (ES6 `const`: no reassignment allowed. Good practice.)
+const ourCommands = require('./commands');
+const chalk = require('chalk');
+const prompt = chalk.blue('\nprompt > ');
 
-// The stdin 'data' event fires after a user types in a line
-// process.stdin.on('data', function (data) {
-//   var cmd = data.toString().trim(); // remove the newline
-//   if(cmd=== 'pwd'){
-//     process.stdout.write('pwd: ' + process.cwd())
-//   } else if (cmd ==='date'){
-//     process.stdout.write('date: ' + new Date().toString())
-//   }
-//   //process.stdout.write('You typed: ' + cmd);
-//   process.stdout.write('\nprompt > ');
+// "state" (persistent but changing information, best kept to a minimum):
+var cmdGroups; // e.g. ['cat bash.js', 'head', 'uniq']
 
+// initial prompt
+process.stdout.write(prompt);
 
-// });
+// STDIN `data` event fires after a user types in a line (inc. newline)
+process.stdin.on('data', function (data) { // data is a Buffer
+  cmdGroups = data.toString().trim().split(/\s*\|\s*/g); // separate by pipe
+  const unsafeCommands = getUnsafeCommands(cmdGroups);
+  if (unsafeCommands.length) {
+    process.stderr.write('command(s) not found: ' + unsafeCommands.join(' '));
+    cmdGroups = [];
+    done('');
+  } else {
+    execute(cmdGroups.shift()); // run the first command-arg set in this line
+  }
+});
 
-var cmds = require('./command.js');
-process.stdin.on('data', cmds.cmdReader);
+function getUnsafeCommands (cmdStrings) {
+  return cmdStrings
+  .map(cmdString => cmdString.split(' ')[0]) // remove arguments (ES6 arrow)
+  .filter(cmd => !ourCommands[cmd]); // filter down to unsafe commands
+}
 
+// run a given command
+function execute (cmdGroup, lastStdout) {
+  const tokens = cmdGroup.toString().trim().split(' '); // separate this cmd and its args
+  const cmd = tokens[0];
+  const args = tokens.slice(1).join(' ');
+  ourCommands[cmd](lastStdout, args, done); // where the magic happens
+}
+
+// handle result of a command (async cannot `return`, only call more funcs)
+function done (stdout) {
+  if (cmdGroups.length) {
+    execute(cmdGroups.shift(), stdout); // execute the next command-arg set
+  } else {
+    process.stdout.write(stdout + prompt);
+  }
+}
